092de394c6b796a3af810b2e56b30119243796e6
EnriquePH <eph.project1500@gmail.com>
2018-02-24 23:18:37 +0100

    lint test

--- tests/testthat/test_check_index_symbol.R
+++ tests/testthat/test_check_index_symbol.R
@@ -3,13 +3,11 @@ index_symbols <- c("US1YI",
                    "USBODI",
                    "USCI")
 
-test_that("Index symbols list is Ok",
-          {
-            testthat::expect_equal(unlist(
-              lapply(index_symbols, check_index_symbol)), index_symbols)
-          })
+test_that("Index symbols list is Ok", {
+  testthat::expect_equal(unlist(lapply(index_symbols, check_index_symbol)),
+                         index_symbols)
+})
 
-test_that("check_index_symbol gives error with wrong code",
-          {
-            testthat::expect_error(check_index_symbol("XXX"))
-          })
+test_that("check_index_symbol gives error with wrong code", {
+  testthat::expect_error(check_index_symbol("XXX"))
+})
